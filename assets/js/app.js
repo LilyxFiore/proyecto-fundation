a1a9729383db4cf4c0a7f939eaf6575d643a76c3
EmmAlva <clioemma_28@hotmail.com>
2017-05-21 10:16:59 -0500

    add font

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,48 @@
-$(document).foundation()
+// $(document).foundation()
+// 
+
+$(document).ready(function(){
+  $('.your-class').slick({
+    setting-name: setting-value
+  });
+});
+
+
+
+$('.autoplay').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 800,
+});
+
+
+$(".slider").slick({
+
+  // normal options...
+  infinite: false,
+
+  // the magic
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+});
