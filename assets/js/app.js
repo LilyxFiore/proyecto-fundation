ffa461701911bb57aaf1428d8dd7266fb6d20eff
EmmAlva <clioemma_28@hotmail.com>
2017-05-22 10:38:30 -0500

    Join menu&slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,20 @@
-// $(document).on('ready', function() {
-//       $(".regular").slick({
-//         dots: true,
-//         infinite: true,
-//         slidesToShow: 5,
-//         slidesToScroll: 5
-//       });
-//       $(".center").slick({
-//         dots: true,
-//         infinite: true,
-//         centerMode: true,
-//         slidesToShow: 5,
-//         slidesToScroll: 5
-//       });
-//       $(".variable").slick({
-//         dots: true,
-//         infinite: true,
-//         variableWidth: true
-//       });
-//     });
+$(document).on('ready', function() {
+       $(".regular").slick({
+         dots: true,
+         infinite: true,
+         slidesToShow: 5,
+         slidesToScroll: 5
+       });
+       $(".center").slick({
+         dots: true,
+         infinite: true,
+         centerMode: true,
+         slidesToShow: 5,
+         slidesToScroll: 5
+       });
+       $(".variable").slick({
+         dots: true,
+         infinite: true,
+         variableWidth: true
+       });
+     });
